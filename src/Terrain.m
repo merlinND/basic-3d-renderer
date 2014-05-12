6e6ed793a10b030cb3a161928d7b6d3fdaf7d60e
Merlin Nimier-David <merlin.nimier@gmail.com>
2014-05-12 14:48:26 +0200

    More small fixes

--- src/Terrain.m
+++ src/Terrain.m
@@ -1,12 +1,13 @@
-%% Array giving the altitude (in meters) of points from a regular grid
-%% with a cell size of 100m:
-%% - x goes from 0 to (13-1)*100 meters
-%% - y goes from 0 to (10-1)*100 meters
-%%
-%% In order to preview this terrain, just run:
-%% > surf(0:100:(13-1)*100,0:100:(10-1)*100,Terr);
+% Array giving the altitude (in meters) of points from a regular grid
+% with a cell size of 100m:
+% - x goes from 0 to (13-1)*100 meters
+% - y goes from 0 to (10-1)*100 meters
+%
+% In order to preview this terrain, just run:
+% > surf(0:100:(13-1)*100,0:100:(10-1)*100, terrain);
 
-Terr=[670   672   670   675   690   680   650   675   690   680   700   892   895;
+terrain = [
+	  670   672   670   675   690   680   650   675   690   680   700   892   895;
       680   665   640   630   650   645   630   628   648   650   680   875   893;
       630   615   585   580   585   600   590   610   603   603   630   850   895;
       595   568   555   560   575   580   575   570   580   610   625   800   850;
@@ -15,5 +16,7 @@ Terr=[670   672   670   675   690   680   650   675   690   680   700   892   89
       545   540   538   597   575   605   593   578   573   593   608   595   695;
       615   560   543   579   569   560   563   570   580   595   619   638   650;
       625   598   560   559   586   558   578   585   600   615   655   680   683;
-      610   600   610   605   615   618   625   638   648   665   680   700   705];
-surf(0:100:(13-1)*100,0:100:(10-1)*100,Terr);
+      610   600   610   605   615   618   625   638   648   665   680   700   705
+];
+
+surf(0:100:(13-1)*100, 0:100:(10-1)*100, terrain);
