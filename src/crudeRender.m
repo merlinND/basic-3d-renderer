37476226f5964721f602fa3b9635a11bb9b8fa4b
Merlin Nimier-David <merlin.nimier@gmail.com>
2014-05-12 17:06:46 +0200

    Debug renderer using Matlab's fill function

---
+++ src/crudeRender.m
@@ -0,0 +1,26 @@
+function crudeRender(triangles, caption)
+%CRUDERENDER Display the given list of trianges using Matlab's fill
+%function
+%   triangles: An n x 6 matrix (three vertex per line)
+%   If the triangles are given in 3D (n x 9), their third coordinate is ignored.
+
+	% Ignore 3D coordinate
+	if (size(triangles, 2) == 9)
+		triangles = [triangles(:, 1:2) triangles(:, 4:5) triangles(:, 7:8)];
+	end;
+	
+	% Number of triangles to render
+	colors = ['r', 'g', 'b'];
+	figure(1);
+	hold on;
+	if (nargin > 1)
+		title(caption)
+	end;
+	
+	for i = 1:size(triangles, 1)
+		triangle = reshape(triangles(i, :), 3, 2);
+		fill(triangle(:, 1), triangle(:, 2), colors(i));
+	end;
+
+end
+
